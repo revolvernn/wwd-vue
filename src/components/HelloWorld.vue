d1b6366353f01e84f57b938ba006bd5723dc229a
revolver <595009116@qq.com>
2019-11-08 16:27:53 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item icon="search">活动</van-tabbar-item>
+    <van-tabbar-item icon="friends-o">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      active: 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
